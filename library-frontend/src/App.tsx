71545e55426bf399d1ce9d3973541e9a122c5bb2
Maciej <mackeplz61@gmail.com>
2024-05-15 22:42:36 +0200

    Dodanie panelu logowania oraz strony wyswietlajacej pozycje dostepne w bibliotece

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
 import Login from './components/login-form/login';
+import BookTable from './components/book-table/bookTable';
 
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
-    </div>
+    <BookTable></BookTable>
   );
 }
 
--- library-frontend/src/components/login-form/login.tsx
+++ library-frontend/src/components/login-form/login.tsx
@@ -1,7 +1,13 @@
-import React, { useCallback, useMemo } from 'react';
+import { useMemo, useCallback } from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
 function Login() {
+  const initialValues = {
+    username: '',
+    password: ''
+  };
+
   const onSubmit = useCallback(
     (values: { username: string; password: string }) => {
       console.log(values);
@@ -20,56 +26,61 @@ function Login() {
 
   return (
     <div className="h-screen flex items-center justify-center transform scale-125 bg-gray-light">
-      <form className="bg-white shadow-xl rounded-lg px-8 pt-6 pb-8 mb-4 bg-blue-200 bg-opacity-100">
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2 text-left"
-            htmlFor="username"
-          >
-            Username
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Username"
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-left text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="******************"
-          />
-          <p className="text-red-500 text-xs italic">
-            Please choose a password.
-          </p>
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-light hover:bg-blue-facebook hover:scale-110 duration-200 ease-in-out text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-          >
-            Sign In
-          </button>
-          <a
-            className="block ml-8 align-baseline font-bold text-sm text-blue-light hover:text-blue-facebook"
-            href="#"
-          >
-            Forgot Password? 
-          </a>
-        </div>
-      </form>
+      <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+        {formik => (
+          <Form className="bg-white shadow-xl rounded-lg px-8 pt-6 pb-8 mb-4 bg-opacity-100">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 text-left"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <Field
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                name="username"
+                placeholder="Username"
+              />
+              <ErrorMessage name="username" component="div" className="text-red-500 text-xs italic" />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-left text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <Field
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                name="password"
+                placeholder="******************"
+              />
+              <ErrorMessage name="password" component="div" className="text-red-500 text-xs italic" />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-light hover:bg-blue-facebook hover:scale-110 duration-200 ease-in-out text-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+                disabled={!formik.isValid || formik.isSubmitting}
+              >
+                Sign In
+              </button>
+              <a
+                className="block ml-8 align-baseline font-bold text-sm text-blue-light hover:text-blue-facebook"
+                href="#"
+              >
+                Forgot Password? 
+              </a>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
 
-//trzeba miec info odnoscnie formularza typu login wymagany i haslo wymagane  i haslo minimum 5 znakow plus guzik powinien sie odpalac tylko w momencie kiedy sa wpisane rzeczy w polach
 export default Login;
